3b728f11297d2b90df32ecd2229286ee002a07d8
Daiki Matsudate <d.matsudate@gmail.com>
2022-06-12 07:14:42 +0900

    Correct localize format

--- Xcodes/Backend/DateFormatter+.swift
+++ Xcodes/Backend/DateFormatter+.swift
@@ -4,15 +4,14 @@ extension DateFormatter {
     /// Date format used in JSON returned from `URL.downloads`
     static let downloadsDateModified: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "MM/dd/yy HH:mm"
-        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = dateFormat(fromTemplate: "MM/dd/yy HH:mm", options: 0, locale: Locale(identifier: "en_US_POSIX"))
         return formatter
     }()
 
     /// Date format used in HTML returned from `URL.download`
     static let downloadsReleaseDate: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM d, yyyy"
+        formatter.dateFormat = dateFormat(fromTemplate: "MMMM d, yyyy", options: 0, locale: Locale.autoupdatingCurrent)
         return formatter
     }()
 }
